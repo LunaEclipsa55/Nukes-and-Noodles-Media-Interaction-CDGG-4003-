c641ecd4d201bfff7531034c52425a8a6a4503b8
LunaEclipsa <Kackah123@icloud.com>
2026-02-27 13:40:52 -0500

    Update Bullets.cs

--- Assets/Scripts/Player/Bullets.cs
+++ Assets/Scripts/Player/Bullets.cs
@@ -25,7 +25,7 @@ public class Bullets : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(1))
         {
             Gun();
         }
