01f59dd3f5fcbe23df9ab76c3836276e054ca8a2
skatermage <magecm09@gmail.com>
2026-04-17 01:41:20 -0400

    Revert "Merge branch 'Enemies' into Production"
    This reverts commit d16c89ad9a2a2ae1873e73ff77bc353659e70a0e, reversing
    changes made to 63d63f721bd6b788d860711a577ba42014b3a956.

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -3,19 +3,14 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public int maxHealth = 100;
-    public int Health;
-    HealthBar healthBar;
-
-    private void Awake()
-    {
-        healthBar =  GetComponentInChildren<HealthBar>();
-    }
+    public int Health = 100;
+    public bool death = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Health = maxHealth;
-        healthBar.UpdateHealthBar(Health, maxHealth);
+        Health = Mathf.Clamp(Health, 0, maxHealth);
+        if (Health == 0) Health = maxHealth;
     }
 
     // Update is called once per frame
@@ -29,15 +24,16 @@ public class EnemyHealth : MonoBehaviour
         if (amount <= 0) return;
 
         Health -= amount;
-        healthBar.UpdateHealthBar(Health, maxHealth);
         if (Health < 0) Health = 0;
+
+        Debug.Log($"{gameObject.name} took {amount} damage.");
 
         if (Health <= 0) Die();
     }
 
     void Die()
     {
-        gameObject.SetActive(false);
+        if(death) gameObject.SetActive(false);
         Debug.Log($"{gameObject.name} dies.");
     }
 }
