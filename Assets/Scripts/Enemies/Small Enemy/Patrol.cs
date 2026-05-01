eb2b586dffd8c681bf9ca5e7d97fa223dd6bd124
skatermage <magecm09@gmail.com>
2026-04-30 23:24:26 -0400

    ahhhhhhh

--- Assets/Scripts/Enemies/Small Enemy/Patrol.cs
+++ Assets/Scripts/Enemies/Small Enemy/Patrol.cs
@@ -9,12 +9,14 @@ public class Patrol : MonoBehaviour
     private bool movingRight = true;
 
     public Transform groundDetection;
+    [SerializeField] private LayerMask groundLayer;
+
 
     private void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
         
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance); // orgin, direction, length
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, groundLayer); // orgin, direction, length
 
         if (groundInfo.collider == false)
         {
